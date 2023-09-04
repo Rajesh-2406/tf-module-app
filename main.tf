826f2b8846cc4df4736a6f037203698c1d8a55eb
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-09-04 10:22:12 +0530

    terraform project

--- main.tf
+++ main.tf
@@ -27,27 +27,27 @@ resource "aws_security_group" "main" {
   }
 }
 resource "aws_lb_target_group" "main" {
-  name = "${var.component}-${var.env}-tg"
-  port = var.app_port
-  protocol = "HTTP"
-  vpc_id = var.vpc_id
+  name      = "${var.component}-${var.env}-tg"
+  port      = var.app_port
+  protocol  = "HTTP"
+  vpc_id    = var.vpc_id
 
   health_check {
-    enabled = true
-    interval = 5
-    path = "/health"
-    port = var.app_port
-    protocol = "HTTP"
-    timeout = 4
-    healthy_threshold = 2
+    enabled             = true
+    interval            = 5
+    path                = "/health"
+    port                = var.app_port
+    protocol            = "HTTP"
+    timeout             = 4
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
 resource "aws_lb_listener_rule" "main" {
   listener_arn      = var.listener_arn
-  priority = var.lb_rule_priority
+  priority          = var.lb_rule_priority
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
   }
   condition {
