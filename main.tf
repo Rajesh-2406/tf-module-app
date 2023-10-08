3343df8edd3bf5a9ad9fe2e7089f405bb2605583
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-08 10:33:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -90,11 +90,11 @@ resource "aws_launch_template" "main" {
 
 }
     resource "aws_autoscaling_group" "main" {
-      desired_capacity = var.desired_capacity
-      max_size = var.max_size
-      min_size = var.min_size
+      desired_capacity    = var.desired_capacity
+      max_size            = var.max_size
+      min_size            = var.min_size
       vpc_zone_identifier = var.subnets
-      target_group_arns = [ aws_lb_target_group.main.arn ]
+      target_group_arns    = [ aws_lb_target_group.main.arn ]
 
 
       launch_template {
