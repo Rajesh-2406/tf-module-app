cfe4c261bb7e5c0384d136c32217a918c1f62052
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-19 10:21:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ resource "aws_launch_template" "main" {
   }
 
  resource "aws_route53_record" "dns" {
-   zone_id = "Z01880432PATWXSDPVK72"
+   zone_id = "Z00116476BN1RJ4Q1IMX"
    name    = local.dns_name
    type    = "CNAME"
    ttl     = 30
